88f6d7fc6314389116d0a1e90075fec072df645b
riyaatole22-prog <riyaatole22@gmail.com>
2026-05-02 20:52:55 +0530

    Refactor getRouter to export router directly

--- src/router.tsx
+++ src/router.tsx
@@ -54,14 +54,13 @@ function DefaultErrorComponent({ error, reset }: { error: Error; reset: () => vo
   );
 }
 
-export const getRouter = () => {
-  const router = createRouter({
-    routeTree,
-    context: {},
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-    defaultErrorComponent: DefaultErrorComponent,
-  });
+export const router = createRouter({
+  routeTree,
+  context: {},
+  scrollRestoration: true,
+  defaultPreloadStaleTime: 0,
+  defaultErrorComponent: DefaultErrorComponent,
+});
 
   return router;
 };
